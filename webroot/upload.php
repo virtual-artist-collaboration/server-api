b6e168d6765efe5a1df574ff76341d39af4af551
hatamasa <masaya19880517@gmail.com>
2019-08-25 11:51:26 +0900

    file upload api作成

--- webroot/upload.php
+++ webroot/upload.php
@@ -1,24 +1,9 @@
 <?php
-// use Firebase\FirebaseLib;
+use Google\Cloud\Storage\StorageClient;
 
-// require_once "../vendor/autoload.php";
-
-// const FIREBASE_CONFIG = [
-//         "apiKey"            => "AIzaSyDPnviB-At7qVa38ouzIb49kTGx7Ije9Cs",
-//         "authDomain"        => "vac-database.firebaseapp.com",
-//         "databaseURL"       => "https://vac-database.firebaseio.com",
-//         "projectId"         => "vac-database",
-//         "storageBucket"     => "",
-//         "messagingSenderId" => "953976227504",
-//         "appId"             => "1:953976227504:web:6c191d6e50d29452",
-//         "databaseSecret"    => "0f4PktuQmeNIPcxbY6maVIzvxGPL6NMHr81Ik0Fn"
-//     ];
-
-// $firebase = new FirebaseLib(FIREBASE_CONFIG["databaseURL"], FIREBASE_CONFIG["databaseSecret"]);
-// $return = $firebase->get("/music");
-
-<?php
 require '../vendor/autoload.php';
+
+ini_set( 'display_errors', 1 );
 
 define("DEFAULT_URL","https://vac-database.firebaseio.com/");
 define("DEFAULT_TOKEN","0f4PktuQmeNIPcxbY6maVIzvxGPL6NMHr81Ik0Fn");
@@ -27,7 +12,7 @@ define("GCS_BUCKETID","vac-storage-1");
 
 // print_r($_FILES);
 
-$storage = new \Google\Cloud\Storage\StorageClient([
+$storage = new StorageClient([
     'projectId' => GCS_PROJECTID,
     'keyFile' => json_decode(file_get_contents('../google.json'), true)
 ]);
@@ -35,11 +20,11 @@ $storage = new \Google\Cloud\Storage\StorageClient([
 $bucket = $storage->bucket(GCS_BUCKETID);
 
 $options = [
-   'name' => basename($_FILES['music']['name'])
+   'name' => basename($_FILES['file']['name'])
 ];
 
 $object = $bucket->upload(
-    fopen($_FILES['music']['tmp_name'], 'r'),
+    fopen($_FILES['file']['tmp_name'], 'r'),
     $options
 );
 
@@ -49,7 +34,7 @@ $data = array(
   "title" => $_POST["title"],
   "description" => $_POST["description"],
   "user_name" => $_POST["user_name"],
-  "url" => $_POST[""],
+  "url" => 'https://storage.googleapis.com/vac-storage-1/'.basename($_FILES['file']['name']),
   "tag_name" => array(),
   "download_count" => 0
 );
@@ -57,5 +42,11 @@ $data = array(
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL,DEFAULT_TOKEN);
 
 $firebase->set("/data/".uniqid("VACD_"),$data);
+
+header("Content-type: application/json; charset=utf-8");
+echo json_encode([
+    'status' => 200,
+    'data' => json_encode($data)
+]);
 
 // $user = $firebase->get("/users");
